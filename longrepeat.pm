a5354e3533ff2d6cb5fa26e95a2065d6034bd411
Toshiyuki SHIMONO <t.shimono@yamanashi.ac.jp>
2024-12-22 19:14:35 +0900

    バージョン0.002として、Changes INSTALL Makefile.PL README longrepeat.pm の5個のファイルを追加。

---
+++ longrepeat.pm
@@ -0,0 +1,25 @@
+package App::longrepeat ;  
+our $VERSION = '0.002' ; 
+our $DATE = '2024-12-22T19:10+09:00' ; 
+
+=encoding utf8
+
+=head1 NAME
+
+App::longrepeat - 何度も出現する全く同じ言い回しを発見する。ただし、改行文字を跨いだ言い回しは対象外。
+
+=head1 SYNOPSIS
+
+This module provides a Unix-like command `F<longrepeat>'. 
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+longrepeat --help
+man longrepeat
+perldoc longrepeat
+
+=cut
+
+1 ;
